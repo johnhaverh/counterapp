1be383baa9579c601925f777b12a7a14d68fb0ed
John Haver Herrera <johnhaverh@gmail.com>
2022-02-04 09:20:58 -0500

    counterapp: main.dart: start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(app)
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text ('Hola Mundo')
+      )     
+    );
+  }
 }
