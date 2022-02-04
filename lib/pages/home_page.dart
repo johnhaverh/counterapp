c2320ab1cfeabad478ed5ca8d66ec6e2ca627f9c
John Haver Herrera <johnhaverh@gmail.com>
2022-02-04 11:12:30 -0500

    counterapp: home_page.dart: update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key})
-      : super(key: key); //replace Key to Hey? for using with NullSafety
+  const HomePage({Key key}) : super(key: key); //replace Key to Hey? for using with NullSafety
 
   @override
   Widget build(BuildContext context) {
     
     const fonsSize30 = TextStyle(fontSize: 30);
+    int counter=0;
 
     return Scaffold(
       appBar: AppBar(
@@ -18,16 +18,18 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, //alinear vertical
           //crossAxisAlignment: CrossAxisAlignment.center, //alinear horizontal
-          children: const [
-            Text('Clicks Counter',style: fonsSize30,),
-            Text('0',style: fonsSize30,),
+          children: [
+            const Text('Clicks Counter',style: fonsSize30,),
+            Text('$counter',style: fonsSize30,),
           ],
         ),
       ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          print("Test Button");          
+          counter++;        
+          print("Counter: $counter");  
         },
       ),
     );
