a3d8495f172d5dc7ebb7801dd050f99cd03fed71
John Haver Herrera <johnhaverh@gmail.com>
2022-02-04 09:51:13 -0500

    counterapp: home_page.dart: rebuild

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key); //replace Key to Hey? for using with NullSafety
+  const HomePage({Key key})
+      : super(key: key); //replace Key to Hey? for using with NullSafety
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('HomePage'));
+    return const Scaffold(
+      body: Center( child: Text('HomePage')),
+    );
   }
 }
