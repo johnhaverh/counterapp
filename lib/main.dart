bdea4045e30cce240980466291486ce842c6efd4
John Haver Herrera <johnhaverh@gmail.com>
2022-02-04 09:42:40 -0500

    counterapp: main.dart: update

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key); //replace Key to Hey? for using with NullSafety
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Center(
-        child: Text ('Hola Mundo')
-      )     
-    );
+        child: Text('Hola Mundo')));
   }
 }
