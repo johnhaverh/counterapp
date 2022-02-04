d386f4b2c07b1e2f2f7c91d70050e023c25e71b8
John Haver Herrera <johnhaverh@gmail.com>
2022-02-04 09:45:38 -0500

    counterapp: main.dart: rebuild

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:counterapp/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(
-        child: Text('Hola Mundo')));
+      home: HomePage()
+    );
   }
 }
